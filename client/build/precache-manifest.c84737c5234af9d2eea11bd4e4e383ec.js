0b79a78c7ac00be0b5b3329ec5faa11e305d29f6
Shubham355 <shubhamg355@gmail.com>
2020-08-21 17:09:25 +0530

    Resolved Deployment issues

---
+++ client/build/precache-manifest.c84737c5234af9d2eea11bd4e4e383ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9400a368a2d582549175e0c773cabc42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c6614872c8bd155cc6d",
+    "url": "/static/css/2.5b21f837.chunk.css"
+  },
+  {
+    "revision": "cbe1dfa786ef7039a23f",
+    "url": "/static/css/main.a3fe3a7b.chunk.css"
+  },
+  {
+    "revision": "8c6614872c8bd155cc6d",
+    "url": "/static/js/2.0640929d.chunk.js"
+  },
+  {
+    "revision": "082e50ca65bb5196a345226be04d8dfb",
+    "url": "/static/js/2.0640929d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbe1dfa786ef7039a23f",
+    "url": "/static/js/main.ffa5bcc6.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "e728604b7079ce6ae27944ba1bce7afc",
+    "url": "/static/media/social_media_2.e728604b.jpg"
+  }
+]);
